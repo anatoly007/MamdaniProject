f641ba29a7b69e848ea271139ed21c0f822c07a9
aser <anatoly-22@mail.ru>
2016-05-06 15:16:25 +0300

    Добавлена треугольная функция принадлежности

--- src/com/spitsin/mamdani/TrianFunc.java
+++ src/com/spitsin/mamdani/TrianFunc.java
@@ -2,20 +2,22 @@ package com.spitsin.mamdani;
 
 import com.spitsin.mamdani.exceptions.ArgumentOutOfBoundsException;
 
-public class TrapFunc implements AccessoryFunc {
+/**
+ * Created by aser on 06.05.2016.
+ */
+public class TrianFunc implements AccessoryFunc{
     private double a;
     private double b;
     private double c;
-    private double d;
     private double activatedValue;
 
-    public TrapFunc(double a, double b, double c, double d) {
+    public TrianFunc(double a, double b, double c) {
         this.a = a;
         this.b = b;
         this.c = c;
-        this.d = d;
     }
 
+    @Override
     public double getValue(double x) {
         if (x <= a) {
             return 0;
@@ -23,40 +25,27 @@ public class TrapFunc implements AccessoryFunc {
         else if (x >= a && x < b) {
             return (x - a) / (b - a);
         }
-        else if (x >= b && x <= c) {
-            return 1;
+        else if (x > b && x <= c) {
+            return (c - x) / (c - b);
         }
-        else if (x > c && x <= d) {
-            return (d - x) / (d - c);
-        }
-        else if (d < x) {
+        else if (c < x) {
             return 0;
         }
         throw new ArgumentOutOfBoundsException();
     }
 
+    @Override
+    public AccessoryFunc copyFunc() {
+        return new TrianFunc(this.a, this.b, this.c);
+    }
+
+    @Override
     public void setActivatedValue(double activatedValue) {
         this.activatedValue = activatedValue;
     }
 
+    @Override
     public double getActivatedValue(double x) {
         return Math.min(getValue(x), activatedValue);
     }
-
-    @Override
-    public String toString() {
-        return "TrapFunc{" +
-                "a=" + a +
-                ", b=" + b +
-                ", c=" + c +
-                ", d=" + d +
-                ", activatedValue=" + activatedValue +
-                '}';
-    }
-
-    public AccessoryFunc copyFunc() {
-        TrapFunc func = new TrapFunc(this.a, this.b, this.c, this.d);
-        return func;
-    }
-
 }
--- src/com/spitsin/mamdani/controller/MamdaniController.java
+++ src/com/spitsin/mamdani/controller/MamdaniController.java
@@ -19,9 +19,9 @@ public class MamdaniController {
         controller.loadVariables();
         controller.loadRules();
 //        double b[] = controller.fuzzification(new double[]{0.7, 0.83});
-//        double b[] = controller.fuzzification(new double[]{0.5, 0.5});
+        double b[] = controller.fuzzification(new double[]{0.8, 0.68});
 
-        double b[] = controller.fuzzification(new double[]{0.12, 0.5, 0.75, 0.95});
+//        double b[] = controller.fuzzification(new double[]{0.12, 0.5, 0.75, 0.95});
         double c[] = controller.aggregation(b);
         System.out.println();
         System.out.println();
@@ -38,58 +38,60 @@ public class MamdaniController {
     private void loadVariables() {
         variables = new HashMap<String, LingVariable>();
 
-//        TermSet ts1 = new TermSet();
-//        ts1.setTerm("низкий", new TrapFunc(0, 0, 0.3, 0.4));
-//        ts1.setTerm("средний", new TrapFunc(0.3, 0.4, 0.6, 0.7));
-//        ts1.setTerm("высокий", new TrapFunc(0.6, 0.7, 1, 1));
-//        LingVariable x1 = new LingVariable("input1", ts1);
-//        variables.put("X1", x1);
-//
-//        TermSet ts2 = new TermSet();
-//        ts2.setTerm("неуд.", new TrapFunc(0, 0, 0.4, 0.5));
-//        ts2.setTerm("уд.", new TrapFunc(0.4, 0.5, 0.6, 0.7));
-//        ts2.setTerm("хор.", new TrapFunc(0.6, 0.7, 0.8, 0.9));
-//        ts2.setTerm("отл.", new TrapFunc(0.8, 0.9, 1, 1));
-//        LingVariable x2 = new LingVariable("input2", ts2);
-//        variables.put("X2", x2);
-//
-//        TermSet ts3 = new TermSet();
-//        ts3.setTerm("низкий", new TrapFunc(0, 0, 0.3, 0.4));
-//        ts3.setTerm("средний", new TrapFunc(0.3, 0.4, 0.7, 0.8));
-//        ts3.setTerm("высокий", new TrapFunc(0.7, 0.8, 1, 1));
-//        LingVariable y = new LingVariable("output", ts3);
-//        variables.put("Y", y);
-
         TermSet ts1 = new TermSet();
-        ts1.setTerm("достоверно", new TrapFunc(0.6, 0.8, 1, 1));
-        ts1.setTerm("недостоверно", new TrapFunc(0, 0, 0.6, 0.8));
-        LingVariable x1 = new LingVariable("Достоверность информации о работодателе", ts1);
+        ts1.setTerm("низкий", new TrapFunc(0, 0, 0.3, 0.4));
+        ts1.setTerm("средний", new TrapFunc(0.3, 0.4, 0.6, 0.7));
+        ts1.setTerm("высокий", new TrapFunc(0.6, 0.7, 1, 1));
+        LingVariable x1 = new LingVariable("input1", ts1);
         variables.put("X1", x1);
 
         TermSet ts2 = new TermSet();
-        ts2.setTerm("надежно", new TrapFunc(0.6, 0.8, 1, 1));
-        ts2.setTerm("ненадежно", new TrapFunc(0, 0, 0.6, 0.8));
-        LingVariable x2 = new LingVariable("Надежность партнерства с работодателем", ts2);
+        ts2.setTerm("неуд.", new TrapFunc(0, 0, 0.4, 0.5));
+        ts2.setTerm("уд.", new TrianFunc(0.4, 0.6, 0.7));
+        ts2.setTerm("хор.", new TrianFunc(0.6, 0.8, 0.9));
+        ts2.setTerm("отл.", new TrianFunc(0.8, 1, 1));
+        LingVariable x2 = new LingVariable("input2", ts2);
         variables.put("X2", x2);
 
         TermSet ts3 = new TermSet();
-        ts3.setTerm("отсутствует", new TrapFunc(0, 0, 0.1, 0.3));
-        ts3.setTerm("частичное", new TrapFunc(0.1, 0.3, 0.7, 0.8));
-        ts3.setTerm("полное", new TrapFunc(0.7, 0.8, 1, 1));
-        LingVariable x3 = new LingVariable("Описание вакансии", ts3);
-        variables.put("X3", x3);
-
-        TermSet ts4 = new TermSet();
-        ts4.setTerm("не обосновано", new TrapFunc(0, 0, 0.5, 0.7));
-        ts4.setTerm("обосновано", new TrapFunc(0.5, 0.7, 1, 1));
-        LingVariable x4 = new LingVariable("Обоснованность количественной потребности", ts4);
-        variables.put("X4", x4);
-
-        TermSet ts5 = new TermSet();
-        ts5.setTerm("недостоверно", new TrapFunc(0, 0, 0.6, 0.8));
-        ts5.setTerm("достоверно", new TrapFunc(0.6, 0.8, 1, 1));
-        LingVariable y = new LingVariable("Показатель достоверности информации", ts5);
+        ts3.setTerm("ноль", new TrapFunc(0, 0, 0.2, 0.3));
+        ts3.setTerm("низкий", new TrapFunc(0.2, 0.3, 0.4, 0.5));
+        ts3.setTerm("средний", new TrapFunc(0.4, 0.5, 0.6, 0.7));
+        ts3.setTerm("высокий", new TrapFunc(0.6, 0.7, 0.8, 0.9));
+        ts3.setTerm("полный", new TrapFunc(0.8, 0.9, 1, 1));
+        LingVariable y = new LingVariable("output", ts3);
         variables.put("Y", y);
+
+//        TermSet ts1 = new TermSet();
+//        ts1.setTerm("достоверно", new TrapFunc(0.6, 0.8, 1, 1));
+//        ts1.setTerm("недостоверно", new TrapFunc(0, 0, 0.6, 0.8));
+//        LingVariable x1 = new LingVariable("Достоверность информации о работодателе", ts1);
+//        variables.put("X1", x1);
+//
+//        TermSet ts2 = new TermSet();
+//        ts2.setTerm("надежно", new TrapFunc(0.6, 0.8, 1, 1));
+//        ts2.setTerm("ненадежно", new TrapFunc(0, 0, 0.6, 0.8));
+//        LingVariable x2 = new LingVariable("Надежность партнерства с работодателем", ts2);
+//        variables.put("X2", x2);
+//
+//        TermSet ts3 = new TermSet();
+//        ts3.setTerm("отсутствует", new TrapFunc(0, 0, 0.1, 0.3));
+//        ts3.setTerm("частичное", new TrapFunc(0.1, 0.3, 0.7, 0.8));
+//        ts3.setTerm("полное", new TrapFunc(0.7, 0.8, 1, 1));
+//        LingVariable x3 = new LingVariable("Описание вакансии", ts3);
+//        variables.put("X3", x3);
+//
+//        TermSet ts4 = new TermSet();
+//        ts4.setTerm("не обосновано", new TrapFunc(0, 0, 0.5, 0.7));
+//        ts4.setTerm("обосновано", new TrapFunc(0.5, 0.7, 1, 1));
+//        LingVariable x4 = new LingVariable("Обоснованность количественной потребности", ts4);
+//        variables.put("X4", x4);
+//
+//        TermSet ts5 = new TermSet();
+//        ts5.setTerm("недостоверно", new TrapFunc(0, 0, 0.6, 0.8));
+//        ts5.setTerm("достоверно", new TrapFunc(0.6, 0.8, 1, 1));
+//        LingVariable y = new LingVariable("Показатель достоверности информации", ts5);
+//        variables.put("Y", y);
     }
 
     private void loadRules() {
@@ -97,100 +99,100 @@ public class MamdaniController {
         rules = new ArrayList<Rule>();
 
 
-        Rule r1 = new Rule();
-        r1.setConditions(new Condition("X1", "недостоверно"));
-        r1.setConclusion(new Conclusion("Y", "недостоверно"));
-        rules.add(r1);
-
-        Rule r2 = new Rule();
-        r2.setConditions(new Condition("X2", "ненадежно"));
-        r2.setConclusion(new Conclusion("Y", "недостоверно"));
-        rules.add(r2);
-
-        Rule r3 = new Rule();
-        r3.setConditions(new Condition("X3", "отсутствует"));
-        r3.setConclusion(new Conclusion("Y", "недостоверно"));
-        rules.add(r3);
-
-        Rule r4 = new Rule();
-        r4.setConditions(new Condition("X4", "обосновано"), new Condition("X3", "частичное"));
-        r4.setConclusion(new Conclusion("Y", "достоверно"));
-        rules.add(r4);
-
-        Rule r5 = new Rule();
-        r5.setConditions(new Condition("X4", "обосновано"), new Condition("X3", "полное"));
-        r5.setConclusion(new Conclusion("Y", "достоверно"));
-        rules.add(r5);
-
-        Rule r6 = new Rule();
-        r6.setConditions(new Condition("X4", "не обосновано"), new Condition("X3", "частичное"));
-        r6.setConclusion(new Conclusion("Y", "недостоверно"));
-        rules.add(r6);
-
-        Rule r7 = new Rule();
-        r7.setConditions(new Condition("X4", "не обосновано"), new Condition("X3", "полное"));
-        r7.setConclusion(new Conclusion("Y", "достоверно"));
-        rules.add(r7);
-
 //        Rule r1 = new Rule();
-//        r1.setConditions(new Condition("X1", "высокий"), new Condition("X2", "отл."));
-//        r1.setConclusion(new Conclusion("Y", "высокий"));
+//        r1.setConditions(new Condition("X1", "недостоверно"));
+//        r1.setConclusion(new Conclusion("Y", "недостоверно"));
 //        rules.add(r1);
 //
 //        Rule r2 = new Rule();
-//        r2.setConditions(new Condition("X1", "высокий"), new Condition("X2", "хор."));
-//        r2.setConclusion(new Conclusion("Y", "высокий"));
+//        r2.setConditions(new Condition("X2", "ненадежно"));
+//        r2.setConclusion(new Conclusion("Y", "недостоверно"));
 //        rules.add(r2);
 //
 //        Rule r3 = new Rule();
-//        r3.setConditions(new Condition("X1", "высокий"), new Condition("X2", "уд."));
-//        r3.setConclusion(new Conclusion("Y", "средний"));
+//        r3.setConditions(new Condition("X3", "отсутствует"));
+//        r3.setConclusion(new Conclusion("Y", "недостоверно"));
 //        rules.add(r3);
 //
 //        Rule r4 = new Rule();
-//        r4.setConditions(new Condition("X1", "высокий"), new Condition("X2", "неуд."));
-//        r4.setConclusion(new Conclusion("Y", "низкий"));
+//        r4.setConditions(new Condition("X4", "обосновано"), new Condition("X3", "частичное"));
+//        r4.setConclusion(new Conclusion("Y", "достоверно"));
 //        rules.add(r4);
 //
 //        Rule r5 = new Rule();
-//        r5.setConditions(new Condition("X1", "средний"), new Condition("X2", "отл."));
-//        r5.setConclusion(new Conclusion("Y", "высокий"));
+//        r5.setConditions(new Condition("X4", "обосновано"), new Condition("X3", "полное"));
+//        r5.setConclusion(new Conclusion("Y", "достоверно"));
 //        rules.add(r5);
 //
 //        Rule r6 = new Rule();
-//        r6.setConditions(new Condition("X1", "средний"), new Condition("X2", "хор."));
-//        r6.setConclusion(new Conclusion("Y", "средний"));
+//        r6.setConditions(new Condition("X4", "не обосновано"), new Condition("X3", "частичное"));
+//        r6.setConclusion(new Conclusion("Y", "недостоверно"));
 //        rules.add(r6);
 //
 //        Rule r7 = new Rule();
-//        r7.setConditions(new Condition("X1", "средний"), new Condition("X2", "уд."));
-//        r7.setConclusion(new Conclusion("Y", "средний"));
+//        r7.setConditions(new Condition("X4", "не обосновано"), new Condition("X3", "полное"));
+//        r7.setConclusion(new Conclusion("Y", "достоверно"));
 //        rules.add(r7);
-//
-//        Rule r8 = new Rule();
-//        r8.setConditions(new Condition("X1", "средний"), new Condition("X2", "неуд."));
-//        r8.setConclusion(new Conclusion("Y", "низкий"));
-//        rules.add(r8);
-//
-//        Rule r9 = new Rule();
-//        r9.setConditions(new Condition("X1", "низкий"), new Condition("X2", "отл."));
-//        r9.setConclusion(new Conclusion("Y", "средний"));
-//        rules.add(r9);
-//
-//        Rule r10 = new Rule();
-//        r10.setConditions(new Condition("X1", "низкий"), new Condition("X2", "хор."));
-//        r10.setConclusion(new Conclusion("Y", "средний"));
-//        rules.add(r10);
-//
-//        Rule r11 = new Rule();
-//        r11.setConditions(new Condition("X1", "низкий"), new Condition("X2", "уд."));
-//        r11.setConclusion(new Conclusion("Y", "низкий"));
-//        rules.add(r11);
-//
-//        Rule r12 = new Rule();
-//        r12.setConditions(new Condition("X1", "низкий"), new Condition("X2", "неуд."));
-//        r12.setConclusion(new Conclusion("Y", "низкий"));
-//        rules.add(r12);
+
+        Rule r1 = new Rule();
+        r1.setConditions(new Condition("X1", "высокий"), new Condition("X2", "отл."));
+        r1.setConclusion(new Conclusion("Y", "полный"));
+        rules.add(r1);
+
+        Rule r2 = new Rule();
+        r2.setConditions(new Condition("X1", "высокий"), new Condition("X2", "хор."));
+        r2.setConclusion(new Conclusion("Y", "высокий"));
+        rules.add(r2);
+
+        Rule r3 = new Rule();
+        r3.setConditions(new Condition("X1", "высокий"), new Condition("X2", "уд."));
+        r3.setConclusion(new Conclusion("Y", "средний"));
+        rules.add(r3);
+
+        Rule r4 = new Rule();
+        r4.setConditions(new Condition("X1", "высокий"), new Condition("X2", "неуд."));
+        r4.setConclusion(new Conclusion("Y", "низкий"));
+        rules.add(r4);
+
+        Rule r5 = new Rule();
+        r5.setConditions(new Condition("X1", "средний"), new Condition("X2", "отл."));
+        r5.setConclusion(new Conclusion("Y", "высокий"));
+        rules.add(r5);
+
+        Rule r6 = new Rule();
+        r6.setConditions(new Condition("X1", "средний"), new Condition("X2", "хор."));
+        r6.setConclusion(new Conclusion("Y", "средний"));
+        rules.add(r6);
+
+        Rule r7 = new Rule();
+        r7.setConditions(new Condition("X1", "средний"), new Condition("X2", "уд."));
+        r7.setConclusion(new Conclusion("Y", "низкий"));
+        rules.add(r7);
+
+        Rule r8 = new Rule();
+        r8.setConditions(new Condition("X1", "средний"), new Condition("X2", "неуд."));
+        r8.setConclusion(new Conclusion("Y", "ноль"));
+        rules.add(r8);
+
+        Rule r9 = new Rule();
+        r9.setConditions(new Condition("X1", "низкий"), new Condition("X2", "отл."));
+        r9.setConclusion(new Conclusion("Y", "средний"));
+        rules.add(r9);
+
+        Rule r10 = new Rule();
+        r10.setConditions(new Condition("X1", "низкий"), new Condition("X2", "хор."));
+        r10.setConclusion(new Conclusion("Y", "низкий"));
+        rules.add(r10);
+
+        Rule r11 = new Rule();
+        r11.setConditions(new Condition("X1", "низкий"), new Condition("X2", "уд."));
+        r11.setConclusion(new Conclusion("Y", "ноль"));
+        rules.add(r11);
+
+        Rule r12 = new Rule();
+        r12.setConditions(new Condition("X1", "низкий"), new Condition("X2", "неуд."));
+        r12.setConclusion(new Conclusion("Y", "ноль"));
+        rules.add(r12);
 
     }
 
